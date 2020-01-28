6608a2896f4ff128be72d7104386effb2f3d0174
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-28 16:07:46 +0700

    Update loadbal.sh

--- loadbal.sh
+++ loadbal.sh
@@ -6,13 +6,10 @@
         echo "Melakukan Instalasi Web server"
         sudo apt-get install -y nginx
         cd /etc/nginx/sites-available
-        sudo rm -f default
-        sudo cat /home/vagrant/project1-cilsy/nginx/default.lb.bak > /etc/nginx/sites-available/default
+        sudo mv default default.bak
+        sudo cp /home/ubuntu/project1-cilsy/nginx/default.lb.bak /etc/nginx/sites-available/default
         echo "restart servoce"
         sudo nginx -t
         sudo nginx -s reload
         sudo systemctl restart nginx.service
         echo "Instal web server selesai"
-        exit 0
-        
-
